4189c4363eb161059c10a7431d4146251aec654f
JavaMetalCoder <zagariafabio1999@gmail.com>
2025-06-27 22:20:10 +0200

    ok

--- script.js
+++ script.js
@@ -1,66 +1,24 @@
-//Fetch API: 
 
-/*
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  .then(response => response.json())
-  .then(data => console.log(data.name)); //"ditto"
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("dataForm");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+  });
 
-
-
-
-
-async function getPokemon() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-  const data = await response.json();
-  console.log(data.name);
-}
-getPokemon();
-
-*/
-
-
-
-
-//Stampa il nome del Pokémon “pikachu” dalla PokeAPI
-
-async function getPikachu() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
-
-  const pikachu = await response.json();
-
-  console.log(pikachu.name);
-  
-}
-
-getPikachu();
-
-//Scrivi una funzione async che prende come parametro il nome di un Pokémon e stampa il suo peso.
-
-async function getWeight(pokemon) {
-  try {
-   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-   if(!response.ok) throw new Error("Pokemon non trovato");
-
-  const data = await response.json();
-  console.log(`${pokemon} pesa ${data.weight}`);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-const text = document.getElementById("nome").value;
+  const text = document.getElementById("nome").value;
 const btn = document.getElementById("btn");
+const responseText = document.getElementById("response").value;
 
 btn.addEventListener("click", getData);
 
 async function getData() {
   try {
-  const response = await fetch('https://jsonplaceholder.typecode.com/posts/1');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
 
-  if(!response.ok) throw new Error("Errore");
+  if(!response.ok) throw new Error(`Errore HTTP: ${response.status}`);
 
-  const data = await response.json;
+  const data = await response.json();
+  responseText.innerText = data.title;
   
   } catch (err) {
     console.log("Errore: " + err.message);
@@ -69,3 +27,5 @@ async function getData() {
 
 }
 
+
+});
